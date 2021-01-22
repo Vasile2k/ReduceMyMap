3923702ff7af569308396efcf6b8667c6ae50fde
Vasile2k <andy.dorcu@yahoo.com>
2021-01-23 01:17:02 +0200

    Input parameters and build output

--- ReduceMyMap.cpp
+++ ReduceMyMap.cpp
@@ -3,7 +3,13 @@
 #include <cassert>
 #include <algorithm>
 #include "mpi.h"
+#include "Master.hpp"
+#include "Servant.hpp"
 
+/*
+ * Parameters are -i for input path and -o for output path
+ * Both are required
+ */
 int main(int argc, char* argv[]) {
 	int rank;
 	int size;
@@ -13,6 +19,15 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	// Nice to run with 9 processes: 1 master and 8 slaves
+	// Rank 0 is always master
+	// Other ranks are ordinary servants
+	if (rank == 0) {
+		Master master(argc, argv);
+		master.run();
+	} else {
+		Servant servant(rank);
+		servant.run();
+	}
 
 	MPI_Finalize();
 	return 0;
